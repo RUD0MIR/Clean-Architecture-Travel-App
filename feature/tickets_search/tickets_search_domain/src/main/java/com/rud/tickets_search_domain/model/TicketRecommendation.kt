a735b468b50e192826e1fc8a0e8c85f25ced4cec
RUD0MIR <rudomir115@gmail.com>
2024-07-19 13:50:07 +0300

    add app icon image, make TicketRecommendation class - data class

--- feature/tickets_search/tickets_search_domain/src/main/java/com/rud/tickets_search_domain/model/TicketRecommendation.kt
+++ feature/tickets_search/tickets_search_domain/src/main/java/com/rud/tickets_search_domain/model/TicketRecommendation.kt
@@ -1,30 +1,8 @@
 package com.rud.tickets_search_domain.model
 
-class TicketRecommendation(
+data class TicketRecommendation(
     val id: Int,
     val price: Int,
     val timeRange: List<String>,
     val title: String
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as TicketRecommendation
-
-        if (id != other.id) return false
-        if (price != other.price) return false
-        if (timeRange != other.timeRange) return false
-        if (title != other.title) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id
-        result = 31 * result + price
-        result = 31 * result + timeRange.hashCode()
-        result = 31 * result + title.hashCode()
-        return result
-    }
-}
+)
